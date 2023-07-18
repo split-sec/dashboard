f1f72c54a54e48628f299ae5adf89f52466ef148
Shyam Venkatesh <shyamvenkat007@gmail.com>
2023-07-18 21:54:42 +0530

    Removed next-auth dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,16 @@ import Layout from "@/components/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
-import SessionProvider from "next-auth";
 
 export default function App({
   Component,
   pageProps,
 }: AppProps) {
   return (
-    // <SessionProvider session={pageProps.session}>
       <ThemeProvider attribute="class">
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
-    // </SessionProvider>
   );
 }
